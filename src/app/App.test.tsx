819639d6452cd1a870fcc5764f646016f0454127
mariano quiroga <mariano.eq@gmail.com>
2023-03-06 19:21:08 +0100

    Remove unnecessary store

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,16 +1,11 @@
 /* eslint-disable testing-library/prefer-screen-queries */
 
 import { render } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { store } from "../store/store.js";
+
 import App from "./App.js";
 
 test("renders learn react link", () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+  const { getByText } = render(<App />);
 
   expect(getByText(/learn/i)).toBeInTheDocument();
 });
